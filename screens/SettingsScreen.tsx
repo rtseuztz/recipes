7bd735080c95282f71f3fbf9385288fc7f2b1866
Saji Shunnarah <sajishunnarah@gmail.com>
2024-01-01 15:10:03 -0500

    logout

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -2,6 +2,7 @@
 // https://aboutreact.com/react-native-bottom-navigation/
 
 import { StackScreenProps } from '@react-navigation/stack';
+import { getAuth, signOut } from 'firebase/auth';
 import * as React from 'react';
 import {
     TouchableOpacity,
@@ -10,57 +11,20 @@ import {
     Text,
     SafeAreaView,
 } from 'react-native';
-
+import { Button } from 'react-native-elements';
+const auth = getAuth();
 const SettingsScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={{ flex: 1, padding: 16 }}>
-                <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <Text
-                        style={{
-                            fontSize: 25,
-                            textAlign: 'center',
-                            marginBottom: 16,
-                        }}>
-                        You are on Setting Screen
-                    </Text>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => navigation.navigate('Home')}>
-                        <Text>Go to Home Tab</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => navigation.navigate('Details')}>
-                        <Text>Open Detail Screen</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => navigation.navigate('Profile')}>
-                        <Text>Open Profile Screen</Text>
-                    </TouchableOpacity>
-                </View>
-                <Text style={{ fontSize: 18, textAlign: 'center', color: 'grey' }}>
-                    React Native Bottom Navigation
-                </Text>
-                <Text style={{ fontSize: 16, textAlign: 'center', color: 'grey' }}>
-                    www.aboutreact.com
-                </Text>
+                <Button title="Log Out" buttonStyle={styles.button} onPress={() => signOut(auth)}></Button>
             </View>
         </SafeAreaView>
     );
 };
 const styles = StyleSheet.create({
     button: {
-        alignItems: 'center',
-        backgroundColor: '#DDDDDD',
         padding: 10,
-        width: 300,
         marginTop: 16,
     },
 });
